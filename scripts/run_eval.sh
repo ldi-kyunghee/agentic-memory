1e179c0356407c26fa3d7630c22eae7430b6b02a
daniazie <dania.moriazi01@gmail.com>
2026-07-14 11:40:56 +0800

    fixed run_eval.sh

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,7 +1,7 @@
 RESULTS=$(ls results)
 
 for file in ${RESULTS[@]}; do
-    if [[ ${file} == *"Qwen"* ]];
+    if [[ ${file} == *"Qwen"* ]]; then
         uv run eval/evaluation.py --results_file ${file};
     fi
 done
