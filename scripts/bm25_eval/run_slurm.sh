02c8a126bdfd72f06164c9fcfc73b8d321ad4fa4
daniazie <dania.moriazi01@gmail.com>
2026-07-16 11:54:13 +0800

    edited slurm script

--- scripts/bm25_eval/run_slurm.sh
+++ scripts/bm25_eval/run_slurm.sh
@@ -10,7 +10,7 @@
 #SBATCH -o ./logs/slurm-%A.out
 #SBATCH -e ./logs/slurm-err-%A.out
 
-tar -xvf dataset/halumem_dataset.tar.gz -C /data2/local_datasets/
+tar -xvf dataset/halumem_dataset.tar.gz -C /local_datasets/
 
 uv run eval/bm25.py --data_path /local_datasets/ --top_k 5 --use_llm --llm_config vllm_config.yaml
 uv run eval/bm25.py --data_path /local_datasets/ --top_k 10 --use_llm --llm_config vllm_config.yaml
