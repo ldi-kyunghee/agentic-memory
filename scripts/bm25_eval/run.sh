eeca1ebebf2242ebe1a0557c519cd1f94baa0f86
daniazie <dania.moriazi01@gmail.com>
2026-07-17 03:39:11 +0800

    edited run.sh

--- scripts/bm25_eval/run.sh
+++ scripts/bm25_eval/run.sh
@@ -1,9 +1,9 @@
-uv run eval/bm25/bm25.py --level per_persona --top_k 5 --use_llm --llm_config vllm_config.yaml
-uv run eval/bm25/bm25.py --level per_session --top_k 5 --use_llm --llm_config vllm_config.yaml
-uv run eval/bm25/bm25.py --level per_persona --top_k 10 --use_llm --llm_config vllm_config.yaml
-# uv run eval/bm25/bm25.py --level per_session --top_k 10 --use_llm --llm_config vllm_config.yaml
+uv run eval/bm25/bm25.py --top_k 5 --use_llm --llm_config vllm_config.yaml
+uv run eval/bm25/bm25.py --top_k 5 --use_llm --llm_config vllm_config.yaml
+uv run eval/bm25/bm25.py --top_k 10 --use_llm --llm_config vllm_config.yaml
+# uv run eval/bm25/bm25.py --top_k 10 --use_llm --llm_config vllm_config.yaml
 
 
-uv run eval/bm25/bm25.py --dataset HaluMem-Long.jsonl --level per_persona --top_k 5 --use_llm --llm_config vllm_config.yaml
-uv run eval/bm25/bm25.py --dataset HaluMem-Long.jsonl --level per_session --top_k 5 --use_llm --llm_config vllm_config.yaml
-uv run eval/bm25/bm25.py --dataset HaluMem-Long.jsonl --level per_persona --top_k 10 --use_llm --llm_config vllm_config.yaml
+uv run eval/bm25/bm25.py --dataset HaluMem-Long.jsonl --top_k 5 --use_llm --llm_config vllm_config.yaml
+uv run eval/bm25/bm25.py --dataset HaluMem-Long.jsonl --top_k 5 --use_llm --llm_config vllm_config.yaml
+uv run eval/bm25/bm25.py --dataset HaluMem-Long.jsonl --top_k 10 --use_llm --llm_config vllm_config.yaml
