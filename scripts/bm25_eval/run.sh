b28b251790b08a02ccc485f1c1dcf78f9b629945
daniazie <dania.moriazi01@gmail.com>
2026-07-17 14:11:58 +0000

    update run.sh

--- scripts/bm25_eval/run.sh
+++ scripts/bm25_eval/run.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=2 bash run_bm25.sh
-CUDA_VISIBLE_DEVICES=2 bash run_eval.sh
+CUDA_VISIBLE_DEVICES=2 bash scripts/bm25_eval/run_bm25.sh
+CUDA_VISIBLE_DEVICES=2 bash scripts/bm25_eval/run_eval.sh
