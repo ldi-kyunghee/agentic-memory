a69298e09c56227989a2a93a86a6f3b5a294d78b
daniazie <dania.moriazi01@gmail.com>
2026-07-27 10:06:51 +0800

    configure llm judge as gpt-5-nano

--- scripts/bm25_eval/run_eval.sh
+++ scripts/bm25_eval/run_eval.sh
@@ -3,6 +3,6 @@ RESULTS=$(ls $RESULTS_DIR)
 
 for file in ${RESULTS[@]}; do
     if [[ ${file} == *"Qwen"* ]]; then
-        uv run eval/bm25/evaluation.py --results_dir $RESULTS_DIR --results_file ${file} --backend vllm --config_file vllm_config.yaml;
+        uv run eval/bm25/evaluation.py --results_dir $RESULTS_DIR --results_file ${file} --backend openai --config_file openai_config.yaml;
     fi
 done
