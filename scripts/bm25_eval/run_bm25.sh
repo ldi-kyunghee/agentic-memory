f90da3c5359a2792501fdaaae12c2afa6b0f449a
daniazie <dania.moriazi01@gmail.com>
2026-07-28 16:04:26 +0800

    update code for hybrid retrieval

--- scripts/bm25_eval/run_bm25.sh
+++ scripts/bm25_eval/run_bm25.sh
@@ -1,2 +1,2 @@
 # uv run eval/bm25/bm25.py --exp_num $1 --top_k 5 --use_llm --llm_config vllm_config.yaml --memory_with_prior_question $2
-uv run eval/bm25/bm25.py --exp_num $1 --top_k 10 --use_llm --llm_config vllm_config.yaml --memory_with_prior_question $2 --hybrid
+uv run eval/bm25/bm25.py --exp_num $1 --top_k 10 --use_llm --llm_config vllm_config.yaml --memory_with_prior_question $2 --hybrid --embed_config embed_config.yaml
