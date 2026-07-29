1ad30c6d4eb111506b523de2af70c2ad57cd060d
daniazie <dania.moriazi01@gmail.com>
2026-07-29 17:23:09 +0800

    update run script

--- scripts/bm25_eval/run_bm25.sh
+++ scripts/bm25_eval/run_bm25.sh
@@ -1,2 +1,3 @@
+BACKEND=$2
 # uv run eval/bm25/bm25.py --exp_num $1 --top_k 5 --use_llm --llm_config vllm_config.yaml --memory_with_prior_question $2
-uv run eval/bm25/bm25.py --exp_num $1 --top_k 10 --use_llm --llm_config vllm_config.yaml --memory_with_prior_question $2 --hybrid --embed_config embed_config.yaml
+uv run eval/bm25/bm25.py --exp_num $1 --top_k 10 --use_llm --backend ${BACKEND} --llm_config ${BACKEND}_config.yaml --memory_with_prior_question $3 --hybrid --embed_config embed_config.yaml
