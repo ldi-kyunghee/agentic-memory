db285c7382cad56d695e9a6beb801809c9432da3
daniazie <dania.moriazi01@gmail.com>
2026-08-04 00:47:34 +0800

    update code

--- scripts/bm25_eval/run_eval.sh
+++ scripts/bm25_eval/run_eval.sh
@@ -1,8 +1,9 @@
 RESULTS_DIR=results/bm25/exp$1/question_answering/
+BACKEND=$2
 RESULTS=$(ls $RESULTS_DIR)
 
 for file in ${RESULTS[@]}; do
     if [[ ${file} == *"Qwen"* ]]; then
-        uv run eval/bm25/evaluation.py --results_dir $RESULTS_DIR --results_file ${file} --backend openai --config_file openai_config.yaml;
+        uv run eval/bm25/evaluation.py --results_dir $RESULTS_DIR --results_file ${file} --backend ${BACKEND} --config_file openai_config.yaml;
     fi
 done
