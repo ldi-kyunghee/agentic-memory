f27c497011fab95631a9a73d5ea8af4e98feeacb
daniazie <dania.moriazi01@gmail.com>
2026-08-05 19:50:11 +0800

    update bash script

--- eval/bm25/evaluation.py
+++ eval/bm25/evaluation.py
@@ -95,7 +95,7 @@ def parse_answers(outputs):
         except JSONDecodeError:
             if content is not None:
                 try:
-                    content = content.split('assistant')[-1]
+                    content = content.split('assistant')[-1].strip().removeprefix("final")
                     result = json.loads(content)
                 except JSONDecodeError:
                     logger.warning("Cannot parse json: %s", content)
