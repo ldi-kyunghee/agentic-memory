bce0c67a76241cd85a6612f9da01bbcdd476fe3b
daniazie <dania.moriazi01@gmail.com>
2026-08-10 10:28:06 +0800

    update code

--- scripts/bm25_eval/run.sh
+++ scripts/bm25_eval/run.sh
@@ -1,2 +1,18 @@
 CUDA_VISIBLE_DEVICES=$1 bash scripts/bm25_eval/run_bm25.sh $2 $3 $4 $5
+
+if [[ $3 == "vllm" ]]; then
+    CUDA_VISIBLE_DEVICES=$1 vllm serve \
+			    openai/gpt-oss-120b \
+			    --port 8000 \
+			    --quantization mxfp4 \
+			    > "gpt-oss-120b.log" 2>&1 &
+    VLLM_PID=$!
+else
+    VLLM_PID=""
+fi
+
 CUDA_VISIBLE_DEVICES=$1 bash scripts/bm25_eval/run_eval.sh $2 $3
+
+if [[ $VLLM_PID != "" ]]; then
+    kill -15 $VLLM_PID
+fi
