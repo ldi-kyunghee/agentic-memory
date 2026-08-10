91f959e7d68ea2926af68c49baa450523b2e3809
daniazie <dania.moriazi01@gmail.com>
2026-08-10 10:28:06 +0800

    update code

--- scripts/bm25_eval/run_eval.sh
+++ scripts/bm25_eval/run_eval.sh
@@ -1,6 +1,6 @@
 RESULTS_DIR=results/bm25/exp$1/question_answering/
 RESULTS=$(ls $RESULTS_DIR)
-BACKEND=openai
+BACKEND=$2
 
 for file in ${RESULTS[@]}; do
     uv run eval/bm25/evaluation.py --results_dir $RESULTS_DIR --results_file ${file} --backend ${BACKEND} --config_file ${BACKEND}_config.yaml;
