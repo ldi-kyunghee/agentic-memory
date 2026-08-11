8d2cc7b6009c86f6aa754d6e4a79ba943cad62ca
daniazie <dania.moriazi01@gmail.com>
2026-08-11 11:23:37 +0800

    update code

--- scripts/bm25_eval/run.sh
+++ scripts/bm25_eval/run.sh
@@ -26,8 +26,6 @@ wait_for_server() {
 }
 
 if [[ $3 == "vllm" ]]; then
-    OPENAI_BASE_URL=http://localhost:8000/v1
-    OPENAI_API_KEY=EMPTY
     CUDA_VISIBLE_DEVICES=$1 uv run vllm serve \
 			    openai/gpt-oss-120b \
 			    --port 8000 \
@@ -36,7 +34,7 @@ if [[ $3 == "vllm" ]]; then
     VLLM_PID=$!
 
     if wait_for_server 8000 "gpt-oss-120b"; then
-	CUDA_VISIBLE_DEVICES=$1 bash scripts/bm25_eval/run_eval.sh $2 $3;
+	OPENAI_BASE_URL="http://localhost:8000/v1" CUDA_VISIBLE_DEVICES=$1 bash scripts/bm25_eval/run_eval.sh $2 $3;
     fi
 
     kill -15 $VLLM_PID
