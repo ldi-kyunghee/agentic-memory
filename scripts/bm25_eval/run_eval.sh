a801ef790c183cafb22402d9d233dba7b94781f5
daniazie <dania.moriazi01@gmail.com>
2026-08-13 11:57:36 +0800

    rename directories and refactor

--- scripts/bm25_eval/run_eval.sh
+++
@@ -1,7 +0,0 @@
-RESULTS_DIR=results/bm25/exp$1/question_answering/
-RESULTS=$(ls $RESULTS_DIR)
-BACKEND=$2
-
-for file in ${RESULTS[@]}; do
-    uv run eval/bm25/evaluation.py --results_dir $RESULTS_DIR --results_file ${file} --backend ${BACKEND} --config_file ${BACKEND}_config.yaml --use_online_inference;
-done
