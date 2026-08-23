1e6a46dcf7395481b90b0f0abff33c13134b8175
daniazie <dania.moriazi01@gmail.com>
2026-08-23 13:43:21 +0800

    fixed code

--- scripts/naive/run_naive.sh
+++ scripts/naive/run_naive.sh
@@ -3,7 +3,7 @@ DATASET=$4
 
 uv run eval/naive/naive_memory.py --exp_num $1 --top_k 5 --memory_with_prior_question $3 --n_persona 4 --dataset ${DATASET} --memory_type bm25
 
-uv run eval/naive/naive_memory.py --exp_num $1 --top_k 5 --with_prior_question $3 --embed_config embed_config.yaml --n_persona 4 --dataset ${DATASET} --memory_type embeddings
+uv run eval/naive/naive_memory.py --exp_num $1 --top_k 5 --memory_with_prior_question $3 --embed_config embed_config.yaml --n_persona 4 --dataset ${DATASET} --memory_type embeddings
 
 uv run eval/naive/naive_memory.py --exp_num $1 --top_k 10 --memory_with_prior_question $3 --memory_type hybrid --embed_config embed_config.yaml --n_persona 4 --dataset ${DATASET}
 
